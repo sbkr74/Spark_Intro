6d333918709bd256fec20913caf7eab7bf555f4a
sbkr74 <sbkr74@gmail.com>
2025-01-02 23:20:33 +0530

    update1.0 (changes for new line)

--- Streaming/python_server.py
+++ Streaming/python_server.py
@@ -1,4 +1,5 @@
 import socket
+import time
 
 # Create a socket server
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -8,7 +9,16 @@ print("Listening on port 9999...")
 
 conn, addr = server.accept()
 print(f"Connection established with {addr}")
-
+count = 0
 while True:
     message = input("Enter message to send: ")
-    conn.sendall(message.encode('utf-8'))
+    conn.sendall((message+"\n").encode('utf-8'))
+    time.sleep(2)
+    count+=1
+    if count == 5:
+        break
+
+# Close the connection
+conn.close()
+server.close()
+print("Server stopped.")
