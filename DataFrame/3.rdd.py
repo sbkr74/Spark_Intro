88c769158edcc8610bcdba9385b46b062d2b13f4
sbkr74 <sbkr74@gmail.com>
2025-01-06 22:53:15 +0530

    printing result

--- DataFrame/3.rdd.py
+++ DataFrame/3.rdd.py
@@ -1,8 +1,9 @@
 from pyspark import SparkContext, SparkConf
 conf = SparkConf().setAppName('rdd example').setMaster('local')
 sc = SparkContext(conf=conf)
-num_list = [3,2,1,4,5,6,9.9,7,8]
+num_list = [3,2,1,4,5,6,9,9,7,8]
 print(type(sc))
 rdd = sc.parallelize(num_list)
 print(type(rdd))
-rdd.collect()
+result = rdd.collect()
+print(result)
