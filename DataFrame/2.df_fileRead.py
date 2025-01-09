4d8153b2b8e64c57fdcf975796cfd6ce0b3a32d0
sbkr74 <sbkr74@gmail.com>
2025-01-09 23:28:20 +0530

    basic operation added

--- DataFrame/2.df_fileRead.py
+++ DataFrame/2.df_fileRead.py
@@ -6,4 +6,25 @@ df = spark.read.format("csv") \
     .option("header","True") \
     .load()
 
-df.show()
+#########################
+# Basic Operation
+# select() ---> to get particular columns from dataframe.
+df1 = df.select("customer id","subscription date","website")
+df1.show(n=5)
+
+# use expr()
+from pyspark.sql.functions import expr
+df1 = df.select("customer id","subscription date","website",expr("date_add(`Subscription Date`,3) as `ext subs`"))
+df1.show(n=5)
+
+# col() --> 
+from pyspark.sql.functions import col
+df2 = df.select(col("Customer ID"),col("Subscription Date"),col("Website"))
+df2.show(n=5)
+
+# selectExpr() --> use both column and expression
+df3 = df.selectExpr("`Customer Id`","`Subscription Date`","Website")
+df3.show(n=5)
+
+df4 = df.selectExpr("`customer id`","`Subscription Date`","website","date_add(`Subscription Date`,3) as `ext subs`")
+df4.show(n=5)
