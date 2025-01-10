f0950bef84a766c3ea55a9b310b79d38bb99126a
sbkr74 <sbkr74@gmail.com>
2025-01-10 12:27:20 +0530

    3.df_column.py file added

---
+++ DataFrame/3.df_column.py
@@ -0,0 +1,26 @@
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.appName('column').getOrCreate()
+
+df = spark.read.format("csv") \
+    .option("path",r"Docs\files\customers-100.csv") \
+    .option("header","True") \
+    .load()
+
+df1 = df.select('customer id','country','subscription date')
+
+# adding Column
+from pyspark.sql.functions import lit,col,date_add,dayofmonth
+df2 = df1.withColumn('destination',lit("India"))
+df2.show(n=5)
+
+# adding column with expression
+df3 = df1.withColumn('new_date',date_add(col('subscription date'),2)).withColumn("Earned_Sal",dayofmonth(col('subscription date')))
+df3.show(n=5)
+
+# printing Schema of dataframe
+df3.printSchema()
+
+# modify existing column
+df4 = df3.withColumn('subscription date',col('subscription date').cast("date"))
+df4.printSchema()
