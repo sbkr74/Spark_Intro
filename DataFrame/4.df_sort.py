70a88d84c634da9aa6d49475fbe18730e6d61c0d
sbkr74 <sbkr74@gmail.com>
2025-01-22 21:04:16 +0530

    update3.0[sorting using col()]

--- DataFrame/4.df_sort.py
+++ DataFrame/4.df_sort.py
@@ -16,3 +16,22 @@ sorted_df.show()
 # multiple column sorting
 mult_sorted_df = df.sort("ProductKey","CustomerName")
 mult_sorted_df.show()
+
+# using col() for better sorting
+from pyspark.sql.functions import col
+
+# single column sorting using col()
+df_col_sort = df.sort(col("ProductKey"))
+df_col_sort.show()
+
+# multiple column sorting using col()
+df_mult_col_sort = df.sort(col("CustomerName"),col("ProductKey"))
+df_mult_col_sort.show()
+
+# single column sorting using col() in descending order
+df_col_sort = df.sort(col("ProductKey").desc())
+df_col_sort.show()
+
+# multiple column sorting using col() in ascending and descending order
+df_mult_col_sort = df.sort(col("CustomerName"),col("ProductKey").asc(),col('SalesAmount').desc())
+df_mult_col_sort.show()
