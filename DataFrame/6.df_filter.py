9ea77d178fc34d1f0acc01b9cb3cacd976494d6a
sbkr74 <sbkr74@gmail.com>
2025-01-29 20:41:27 +0530

    6.df_filter.py file added

---
+++ DataFrame/6.df_filter.py
@@ -0,0 +1,31 @@
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.appName('filter operation').getOrCreate()
+
+file_path = r"Docs\files\person_data_quality.csv"
+df = spark.read.option("Header","True").option("inferSchema","True").csv(file_path)
+
+# for string type
+filter_df = df.filter("Name != 'NULL'")
+filter_df.show()
+
+# for integer type
+filter_df = df.filter("Age == 50")
+filter_df.show()
+
+# Using Col function
+from pyspark.sql.functions import col
+filter_df = df.filter(col("Phone") != 'NULL')
+filter_df.show()
+
+# for mutiple comdition
+filter_df = df.filter("Age == 50" or "Age == 30")
+filter_df.show()
+
+filter_df = df.filter("Age == 50" and "Name == 'NULL'")
+filter_df.show()
+
+
+# Using col for different operations
+filter_df = df.filter(col("Age").between(30,50))
+filter_df.show()
