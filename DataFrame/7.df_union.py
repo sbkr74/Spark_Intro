75ce5fac6f1e97a9ac47f3bdbae1ef68867af255
sbkr74 <sbkr74@gmail.com>
2025-01-29 21:48:57 +0530

    update1.0[misaligned column]

--- DataFrame/7.df_union.py
+++ DataFrame/7.df_union.py
@@ -1,4 +1,6 @@
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import col
+
 
 spark = SparkSession.builder.appName('filter operation').getOrCreate()
 
@@ -11,3 +13,15 @@ df_bottom = df.orderBy("Record ID",ascending=False).limit(5)
 df_union = df_top.union(df_bottom)
 df_union.show()
 
+# If column names are not aligned
+df_union = df_top.unionByName(df_bottom)
+df_union.show()
+
+df1 = df.select("Name","Age","Email").limit(10)
+df2 = df.select("Age","Name","Email").orderBy(col("Record Id").desc()).limit(5)
+
+df_union = df1.union(df2)           # problem with union if column names are not aligned
+df_union.show()
+
+df_union = df1.unionByName(df2)           
+df_union.show()
