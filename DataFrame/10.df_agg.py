e4a7d4cabd43daf498aa24cefda1eac2a73843f7
sbkr74 <sbkr74@gmail.com>
2025-02-05 20:43:52 +0530

    update1.0(all basic agg func)

--- DataFrame/10.df_agg.py
+++ DataFrame/10.df_agg.py
@@ -1,5 +1,5 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import *
+from pyspark.sql.functions import sum,avg,min,max,count,countDistinct
 
 # Creating SparkSession
 spark = SparkSession.builder \
@@ -16,8 +16,44 @@ df = spark.read.option("header","True") \
 # Use of Agg functions
 # Use of sum()
 df_sum = df.select(sum("SalesAmount"))
-df_sum.show(5)
+df_sum.show()
 
 # Use of sum() with alias
 df_sum = df.select(sum("SalesAmount").alias("TotalSales"))
-df_sum.show(5)
+df_sum.show()
+
+# Use of avg()
+df_avg = df.select(avg("SalesAmount"))
+df_avg.show()
+
+# Use of avg() with round and alias
+df_avg = df.select(round(avg("SalesAmount"),2).alias("AvgSales"))
+df_avg.show()
+
+# Use of min()
+df_min = df.select(min("SalesAmount"))
+df_min.show()
+
+# Use of min() with alias
+df_min = df.select(min("SalesAmount").alias("MinSales"))
+df_min.show()
+
+# Use of max()
+df_max = df.select(max("SalesAmount"))
+df_max.show()
+
+# Use of max() with alias
+df_max= df.select(max("SalesAmount").alias("MaxSales"))
+df_max.show()
+
+# Use of count()
+df_count = df.select(count("SalesAmount"))
+df_count.show()
+
+# Use of count() with alias
+df_count_dist = df.select(count("SalesAmount").alias("NumOfSales"))
+df_count_dist.show()
+
+# Use of countDistinct() with alias
+df_count_dist = df.select(countDistinct("Country").alias("UniqueCont"))
+df_count_dist.show()
