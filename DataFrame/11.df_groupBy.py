8ad13f9d772420deb9866bb619c510fb5235be81
sbkr74 <sbkr74@gmail.com>
2025-02-05 23:05:38 +0530

    update1.0(multiple col agg and grpBy)

--- DataFrame/11.df_groupBy.py
+++ DataFrame/11.df_groupBy.py
@@ -16,3 +16,13 @@ df = spark.read.option("header","True") \
 df_grp = df.groupBy("Country").sum("SalesAmount").withColumnRenamed("sum(SalesAmount)", "TotalSales")
 df_grp.show()
 
+# multiple agg columns
+df_grp = df.groupBy("Country").sum("SalesAmount", "TaxAmt") \
+    .withColumnRenamed("sum(SalesAmount)", "TotalSales") \
+    .withColumnRenamed("sum(TaxAmt)", "TotalTax")
+
+df_grp.show()
+
+# multiple groupBy columns
+df_grp = df.groupBy("Country","ProductName").sum("SalesAmount")
+df_grp.show()
